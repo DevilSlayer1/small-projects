32fd8f053a3320dfa231319ce9878de9b491e4b6
DevilSlayer1 <153759200+DevilSlayer1@users.noreply.github.com>
2024-02-16 14:18:16 +0000

    added favicon

--- Basic calculator/index.js
+++ Basic calculator/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port=8000;
+const port=8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
